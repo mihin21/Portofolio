1e97a5b6f8469de6f6312ae82cb61a4c5e7109a3
mihin21 <huguesaime.mihin@gmail.com>
2021-10-26 12:31:05 -0700

    heroku configuration

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -5,29 +5,29 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Dashboard | Portofolio MHA</title>
-    <link rel="stylesheet" href="{{asset('assets/vendors/mdi/css/materialdesignicons.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/vendors/css/vendor.bundle.base.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/vendors/jvectormap/jquery-jvectormap.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/vendors/flag-icon-css/css/flag-icon.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/vendors/owl-carousel-2/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/vendors/owl-carousel-2/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset_secure('assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset_secure('assets/vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet" href="{{asset_secure('assets/vendors/jvectormap/jquery-jvectormap.css')}}">
+    <link rel="stylesheet" href="{{asset_secure('assets/vendors/flag-icon-css/css/flag-icon.min.css')}}">
+    <link rel="stylesheet" href="{{asset_secure('assets/vendors/owl-carousel-2/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset_secure('assets/vendors/owl-carousel-2/owl.theme.default.min.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset_secure('assets/css/style.css')}}">
   </head>
   <body>
 
      @yield('contenu')
 
-    <script src="{{asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
-    <script src="{{asset('assets/vendors/chart.js/Chart.min.js')}}"></script>
-    <script src="{{asset('assets/vendors/progressbar.js/progressbar.min.js')}}"></script>
-    <script src="{{asset('assets/vendors/jvectormap/jquery-jvectormap.min.js')}}"></script>
-    <script src="{{asset('assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-    <script src="{{asset('assets/vendors/owl-carousel-2/owl.carousel.min.js')}}"></script>
-    <script src="{{asset('assets/js/off-canvas.js')}}"></script>
-    <script src="{{asset('assets/js/hoverable-collapse.js')}}"></script>
-    <script src="{{asset('assets/js/misc.js')}}"></script>
-    <script src="{{asset('assets/js/settings.js')}}"></script>
-    <script src="{{asset('assets/js/todolist.js')}}"></script>
-    <script src="{{asset('assets/js/dashboard.js')}}"></script>
+    <script src="{{asset_secure('assets/vendors/js/vendor.bundle.base.js')}}"></script>
+    <script src="{{asset_secure('assets/vendors/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset_secure('assets/vendors/progressbar.js/progressbar.min.js')}}"></script>
+    <script src="{{asset_secure('assets/vendors/jvectormap/jquery-jvectormap.min.js')}}"></script>
+    <script src="{{asset_secure('assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+    <script src="{{asset_secure('assets/vendors/owl-carousel-2/owl.carousel.min.js')}}"></script>
+    <script src="{{asset_secure('assets/js/off-canvas.js')}}"></script>
+    <script src="{{asset_secure('assets/js/hoverable-collapse.js')}}"></script>
+    <script src="{{asset_secure('assets/js/misc.js')}}"></script>
+    <script src="{{asset_secure('assets/js/settings.js')}}"></script>
+    <script src="{{asset_secure('assets/js/todolist.js')}}"></script>
+    <script src="{{asset_secure('assets/js/dashboard.js')}}"></script>
   </body>
